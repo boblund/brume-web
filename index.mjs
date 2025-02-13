4d297c28b5ea817098ee182ea53521a6a7260806
Bob <bob.lund@gmail.com>
2025-02-12 20:23:09 -0700

    reorder export

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-export { dialog, getToken };
 import { dialog } from './dialog.mjs';
 import { getToken } from './brumeLogin.mjs';
 import './brume-elements.mjs';	// only side effects
+export { dialog, getToken };
