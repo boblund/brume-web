f0cf650593aa134f58b1f2046ae25c6169c9ffdf
Bob <bob.lund@gmail.com>
2025-07-21 19:34:21 -0600

    elements first

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
+import './brume-elements.mjs';	// only side effects
 import { dialog } from './dialog.mjs';
 import { getToken } from './brumeLogin.mjs';
-import './brume-elements.mjs';	// only side effects
 export { dialog, getToken };
